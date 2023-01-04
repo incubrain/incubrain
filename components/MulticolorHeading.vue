e9f0a8340d68681ca769c77be1be8fbca713f966
Drew MacGibbon <drewmacgibbon@gmail.com>
2023-01-05 01:13:53 +1300

    docs: backend zod example

--- components/MulticolorHeading.vue
+++ components/MulticolorHeading.vue
@@ -8,22 +8,6 @@
 
 <script setup lang="ts">
 
-// interface Props {
-//   src?: string
-//   width?: string
-//   height?: string
-//   speed?: number
-//   mode?: string
-// }
-
-// const props = withDefaults(defineProps<Props>(), {
-//   src: 'https://assets10.lottiefiles.com/packages/lf20_soCRuE.json',
-//   width: '100%',
-//   height: '100%',
-//   speed: 1,
-//   mode: 'bounce'
-// })
-
 </script>
 
 <style>
