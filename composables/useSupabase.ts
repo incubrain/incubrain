64d7d252ebbaf7eb1ff349bdb9f50661763813e7
Drew MacGibbon <drewmacgibbon@gmail.com>
2023-07-16 01:32:00 +0530

    fix(websocket): remove unused todos ws

--- composables/useSupabase.ts
+++ composables/useSupabase.ts
@@ -8,15 +8,8 @@ import { createClient } from '@supabase/supabase-js'
 
 export default function useSupabase () {
   const supabase = createClient(process.env.SUPABASE_URL!, process.env.SUPABASE_KEY!)
-  const posts = ref({})
-  // Create a function to handle inserts
-  const handleInserts = (payload) => {
-    console.log('Change received!', payload)
-  }
-  const { data: todos, error } = await supabase.from('todos')?.on('INSERT', handleInserts).subscribe()
 
   return {
-    client: supabase,
-    posts: computed(() => posts.value)
+    client: supabase
   }
 }
