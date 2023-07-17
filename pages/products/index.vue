59e20037fb54c68e60fa2df96478d34ba2138cf7
Drew MacGibbon <drewmacgibbon@gmail.com>
2023-07-17 23:24:04 +0530

    feat(page): products page

---
+++ pages/products/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <ProductsCard />
+  </div>
+</template>
+
+<script setup lang="ts">
+// import services from '@/data/products.json'
+
+// let service = ref(null)
+
+// onMounted(async () => {
+//   const route = useRoute()
+//   const serviceId = route.params.id
+//   service.value = services.find(s => s.id === serviceId)
+// })
+
+</script>
+
+<style scoped>
+
+</style>
