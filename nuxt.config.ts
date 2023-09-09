7ada729bc6741f1885f0f85da27c9a8f25507dcb
Drew MacGibbon <drewmacgibbon@gmail.com>
2023-09-09 11:10:24 +0530

    package(update): nuxthq/ui to @nuxt/ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,7 +22,7 @@ export default defineNuxtConfig({
     '@nuxt/content',
     '@unlighthouse/nuxt',
     '@nuxt/image',
-    '@nuxthq/ui',
+    '@nuxt/ui',
     'nuxt-swiper',
     '@vee-validate/nuxt',
     [
