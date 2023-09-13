926c59e73aaa80a85e2dded8921880482b45bc82
Drew MacGibbon <drewmacgibbon@gmail.com>
2023-09-13 20:50:56 +0530

    conf(ui): styled nuxt/ui components

--- app.config.ts
+++ app.config.ts
@@ -1,8 +1,14 @@
 export default defineAppConfig({
   title: 'Incubrain',
   ui: {
+    primary: 'emerald',
     slideover: {
       background: 'background'
+    },
+    card: {
+      background: 'foreground',
+      ring: 'ring-1 ring-color',
+      divide: 'divide-y divide-color'
     },
     notifications: {
       position: 'bottom-1 right-1'
@@ -12,6 +18,55 @@ export default defineAppConfig({
       ring: '',
       item: {
         active: 'foreground'
+      }
+    },
+    accordion: {
+      wrapper: ''
+    },
+    input: {
+      color: {
+        white: {
+          outline:
+            'foreground shadow-sm ring-1 ring-inset ring-gray-300 dark:ring-gray-700 focus:ring-2 focus:ring-primary-500 dark:focus:ring-primary-400'
+        }
+      }
+    },
+    textarea: {
+      color: {
+        white: {
+          outline:
+            'foreground shadow-sm ring-1 ring-inset ring-gray-300 dark:ring-gray-700 focus:ring-2 focus:ring-primary-500 dark:focus:ring-primary-400'
+        }
+      }
+    },
+    select: {
+      color: {
+        white: {
+          outline:
+            'foreground shadow-sm ring-1 ring-inset ring-gray-300 dark:ring-gray-700 focus:ring-2 focus:ring-primary-500 dark:focus:ring-primary-400'
+        }
+      },
+      default: {
+        size: 'md'
+      }
+    },
+    selectMenu: {
+      padding: 'p-2',
+      input: 'foreground',
+      background: 'foreground',
+      option: {
+        active: 'background'
+      }
+    },
+    radio: {
+      background: 'foreground'
+    },
+    tabs: {
+      list: {
+        background: 'foreground',
+        marker: {
+          background: 'background'
+        }
       }
     }
   }
