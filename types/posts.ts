d418e7abc37399d0b01dfa0a122732d1c8b6ef0d
Drew MacGibbon <drewmacgibbon@gmail.com>
2023-10-12 19:35:22 +0530

    types: add category/tag enum

--- types/posts.ts
+++ types/posts.ts
@@ -1,3 +1,25 @@
 import { z } from 'zod'
 
-export const postCardSchema = z.object({})
+export const postCategorySchema = z.enum(['all', 'frontend', 'backend', 'business', 'projects'])
+export const postTagSchema = z.enum([
+  // frontend
+  'nuxt',
+  'vue',
+  'typescript',
+  // backend
+  'nitro',
+  'supabase',
+  'postgresql',
+  'auth',
+  'ci/cd',
+  // design
+  'tailwindcss',
+  // general
+  'learning',
+  'code quality',
+  'testing',
+  'productivity'
+])
+
+export type PostCategories = z.infer<typeof postCategorySchema>
+export type PostTags = z.infer<typeof postTagSchema>
