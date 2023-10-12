0459e996fda288774ddab492e39ab010ba5dff0d
Drew MacGibbon <drewmacgibbon@gmail.com>
2023-10-12 15:07:53 +0530

    feat(blog): scrollToTop on category change

--- components/blog/BlogFilter.vue
+++ components/blog/BlogFilter.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="lg:sticky top-24 space-y-4 h-auto border border-color rounded-md p-4 xl:p-8">
-    <h3 class="text-lg font-semibold">Filters</h3>
     <div class="space-y-2">
-      <p class="text-xs">Categories:</p>
+      <p class="text-sm">Categories</p>
       <div class="flex gap-2 flex-wrap">
         <UButton
           v-for="cat in categories"
@@ -12,11 +11,14 @@
           :label="cat"
           size="sm"
           class="cursor-pointer"
-          @click="filter.toggleCategory(cat)"
+          @click="
+            postStore.toggleCategory(cat);
+            toTop()
+          "
         />
       </div>
     </div>
-    <div class="space-y-2">
+    <!-- <div class="space-y-2">
       <p class="text-xs">Tags:</p>
       <div class="flex gap-2 flex-wrap">
         <UButton
@@ -33,16 +35,20 @@
               sm: 'py-1 px-2',
             }
           }"
-          @click="filter.toggleTag(tag)"
+          @click="postStore.toggleTag(tag)"
         />
       </div>
-    </div>
+    </div> -->
   </div>
 </template>
 
 <script setup lang="ts">
+const postStore = usePostsStore()
+const { selectedCategory, categories } = storeToRefs(postStore)
 
-const filter = useFiltersStore()
-const { selectedCategory, selectedTags, tags, categories } = storeToRefs(filter)
-
+function toTop() {
+  if (window) {
+    window.scrollTo({ top: 0, behavior: 'smooth' })
+  }
+}
 </script>
