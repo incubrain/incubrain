e950e1d0fe2193c0578e8bfa8d57efa359ca6d6d
Drew MacGibbon <drewmacgibbon@gmail.com>
2023-10-25 22:52:36 +0530

    ui: improve ceo message & add posthog tracking

--- components/common/CommonCeoMessage.vue
+++ components/common/CommonCeoMessage.vue
@@ -1,24 +1,75 @@
 <template>
   <div class="grid grid-cols-1 lg:grid-cols-2 gap-8 items-center justify-center">
     <CommonTitle :title="title" />
-    <div class="gap-3 flex flex-col w-full items-center">
+    <div class="gap-3 flex flex-col w-full items-start">
       <div class="relative w-full flex justify-center items-center">
         <div
-          class="flex w-[600px] lg:w-full h-full relative rounded-[90%_40%] overflow-hidden justify-center items-center shadow-md border-2 border-color"
+          class="flex w-[600px] gap-4 pt-12 px-6 lg:w-full h-full relative rounded-md border-4 border-primary-600 dark:border-primary-900 overflow-hidden justify-around items-start shadow-md"
         >
           <div class="bg-pattern z-0" />
+          <div class="flex flex-col gap-4 text-base relative font-semibold bg-white/80">
+            <h4 class="text-xl font-bold">Reach out!</h4>
+            <NuxtLink
+              class="flex gap-1 items-center"
+              :to="discord.url"
+              @click="
+                $posthog()?.capture('join_community', {
+                  source: 'ceo_message'
+                })
+              "
+            >
+              <UIcon name="i-mdi-discord" />
+              Nuxt Founder Group
+            </NuxtLink>
+            <NuxtLink
+              class="flex gap-1 items-center"
+              :to="personalLinkedin.url"
+              @click="
+                $posthog()?.capture('view_linkedin', {
+                  source: 'ceo_message'
+                })
+              "
+            >
+              <UIcon name="i-mdi-linkedin" />
+              Personal Linkedin
+            </NuxtLink>
+            <NuxtLink
+              class="flex gap-1 items-center"
+              :to="personalGithub.url"
+              @click="
+                $posthog()?.capture('view_github', {
+                  source: 'ceo_message'
+                })
+              "
+            >
+              <UIcon name="i-mdi-github" />
+              Personal Github
+            </NuxtLink>
+            <NuxtLink
+              class="flex gap-1 items-center"
+              to="mailto:mac@incubrain.org"
+              @click="
+                $posthog()?.capture('send_email', {
+                  source: 'ceo_message'
+                })
+              "
+            >
+              <UIcon name="i-mdi-email" />
+              Email
+            </NuxtLink>
+          </div>
           <NuxtImg
             src="images/team/drew-macgibbon.png"
             alt="Drew MacGibbon Ceo/Founder of Incubrain"
             height="439"
             width="228"
-            class="relative"
+            class="relative grayscale-[60%]"
           />
         </div>
       </div>
       <div>
         <h4 class="font-semibold">Drew MacGibbon</h4>
-        <p>CEO/Founder, Incubrain</p>
+        <p>Incubrain CEO/Founder</p>
       </div>
     </div>
   </div>
@@ -26,6 +77,8 @@
 
 <script setup lang="ts">
 import { Title } from '~/types/content'
+
+const { discord, personalGithub, personalLinkedin } = useSocial()
 
 defineProps({
   title: {
--- composables/useSocial.ts
+++ composables/useSocial.ts
@@ -9,14 +9,28 @@ const links = [
   {
     id: 0,
     label: 'Discord',
-    icon: 'i-mdi-chevron-right',
+    icon: 'i-mdi-discord',
     url: 'https://discord.gg/zSGHvNZXAa'
+  },
+  {
+    id: 1,
+    label: 'Personal Github',
+    icon: 'i-mdi-github',
+    url: 'https://github.com/Drew-Macgibbon'
+  },
+  {
+    id: 2,
+    label: 'Personal Linkedin',
+    icon: 'i-mdi-linkedin',
+    url: 'https://www.linkedin.com/in/drew-macgibbon'
   }
 ] as Link[]
 
 export default function useSocial() {
   return {
-    discord: links.filter((link) => link.label === 'Discord')[0] as Link,
+    discord: links.filter((link) => link.id === 0)[0] as Link,
+    personalGithub: links.filter((link) => link.id === 1)[0] as Link,
+    personalLinkedin: links.filter((link) => link.id === 2)[0] as Link,
     links
   }
 }
