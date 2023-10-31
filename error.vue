253e481d55c7af45c37063a6a29bf06261e13625
Drew MacGibbon <drewmacgibbon@gmail.com>
2023-10-31 13:34:44 +0530

    fix: ignore certain routes on generate

--- error.vue
+++ error.vue
@@ -1,9 +1,17 @@
 <template>
   <div class="h-screen w-full wrapper">
-    <div class="w-full h-full flex flex-col gap-4 items-center justify-center leading-none max-w-xl mx-auto">
+    <div
+      class="w-full h-full flex flex-col gap-8 items-center justify-center leading-none max-w-xl mx-auto"
+    >
       <h2 class="text-3xl font-semibold">Route {{ error.url }} returned {{ error.statusCode }}</h2>
       <p class="text-xl">{{ error.message }}</p>
-      <UAlert color="red" variant="soft" title="Stack Trace" :description="error.stack" />
+      <UAlert
+        v-if="error.stack"
+        color="red"
+        variant="soft"
+        title="Stack Trace"
+        :description="error.stack"
+      />
       <UButton
         size="lg"
         @click="handleError"
---
+++ pages/_dev.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="wrapper padded-y padded-x">
+    <CommonDivider :title="title" />
+  </div>
+</template>
+
+<script setup lang="ts">
+
+const title = {
+  main: 'Strapped For Cash?',
+  subtitle: 'Join our community to get FREE advice!'
+}
+
+</script>
+
+<style scoped></style>
