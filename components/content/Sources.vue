4a92030bf54af1f9b4cd148e291eb74e9234a8a3
Drew MacGibbon <drewmacgibbon@gmail.com>
2023-11-18 14:21:38 +0530

    feat(blog): FAQ & Sources components

---
+++ components/content/Sources.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="py-4">
+    <table class="w-full overflow-hidden border-collapse">
+      <thead>
+        <tr class="foreground">
+          <th class="border border-color p-2 text-left font-semibold">Title</th>
+          <th class="border border-color p-2 text-left font-semibold">About</th>
+          <th class="border border-color p-2 text-left font-semibold">Link</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="(value, index) in sources"
+          :key="index"
+          class="text-sm odd:background even:foreground hover:bg-primary-50 dark:hover:bg-primary-950"
+        >
+          <td class="border border-color p-2">{{ value.title }}</td>
+          <td class="border border-color p-2">{{ value.about }}</td>
+          <td class="border border-color p-2">
+            <UButton
+              :to="value.link"
+              target="_blank"
+              icon="i-material-symbols-source-notes"
+              variant="link"
+              rel="noopener noreferrer"
+              class="whitespace-nowrap"
+            />
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script setup lang="ts">
+type Source = {
+  title: string
+  about: string
+  link: string
+}
+
+defineProps<{
+  sources: Source[]
+}>()
+</script>
